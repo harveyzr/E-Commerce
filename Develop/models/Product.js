81420beb9bf2e2410d418136255ac00b9488c548
harveyzr <harveyzr@gmail.com>
2024-05-29 19:30:02 -0400

    prodcuttag

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,15 +9,66 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+     },
 
-module.exports = Product;
+     product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+     },
+    
+     price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+     },
+     
+     stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10, 
+      validate: {
+        isNumeric: true
+      }
+     },
+
+     category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
+     }
+    },
+
+    {
+      sequelize, 
+      timestamps: false,
+      freezeTabelName: true, 
+      underscored: true, 
+      modelName: 'product',
+    }
+  );
+   
+  module.exports = Product;
+
+
+
+     
+      
+       
+  
+  
+  
+  
+  
+
+
+
+
